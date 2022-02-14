3b55cd0d5d6be47fc5cf8e912872cfb1b05ef4f8
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-02-14 19:24:35 +0700

    install auth, user & admin

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
       <nav class="row navbar navbar-expand-lg navbar-light bg-white">
           <a href="#" class="navbar-brand">
-              <img src="frontend/images/logo.png" alt="Logo Wisata">
+              <img src="{{ url('frontend/images/logo.png') }}" alt="Logo Wisata">
           </a>
           <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
               <span class="navbar-toggler-icon"></span>
@@ -33,19 +33,42 @@
 
               </ul>
 
-              <!-- Mobile Button -->
-              <form class="form-inline d-sm-block d-md-none">
-                  <button class="btn btn-login my-2 my-sm-0">
-                      Masuk
-                  </button>
-              </form>
+              @guest
+                  <!-- Mobile Button -->
+                  <form class="form-inline d-sm-block d-md-none">
+                      <button class="btn btn-login my-2 my-sm-0" tpye="button"
+                          onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                          Masuk
+                      </button>
+                  </form>
 
-              <!-- Desktop Button -->
-              <form class="form-inline my-2 my-lg-0 d-none d-md-block">
-                  <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-                      Masuk
-                  </button>
-              </form>
+                  <!-- Desktop Button -->
+                  <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                      <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" tpye="button"
+                          onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                          Masuk
+                      </button>
+                  </form>
+              @endguest
+
+              @auth
+                  <!-- Mobile Button -->
+                  <form class="form-inline d-sm-block d-md-none" action="{{ url('logout') }}" method="post">
+                      @csrf
+                      <button class="btn btn-login my-2 my-sm-0" type="submit">
+                          keluar
+                      </button>
+                  </form>
+
+                  <!-- Desktop Button -->
+                  <form class="form-inline my-2 my-lg-0 d-none d-md-block" action="{{ url('logout') }}" method="post">
+                      @csrf
+                      <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+                          keluar
+                      </button>
+                  </form>
+              @endauth
+
           </div>
       </nav>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\Admin\DashboardController;
-
-
-
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +34,13 @@ Route::get('/checkout/success', [CheckoutController::class, 'success'])
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])
             ->name('dashboard');
     });
+
+Auth::routes(['verify' => true]);
+
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
