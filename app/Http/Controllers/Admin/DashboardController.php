86dd0001d02d5b83962c78c754d888dc94669a24
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-02-17 14:27:09 +0700

    Data Statistik

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\TravelPackage;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        // Cara menghitung total data yg ada didalam db, dan berfungsi untuk membuat dashboard
+        return view('pages.admin.dashboard', [
+            'travel_package' => TravelPackage::count(),
+            'transaction' => Transaction::count(),
+            'transaction_pending' => Transaction::where('transaction_status', 'PENDING')->count(),
+            'transaction_success' => Transaction::where('transaction_status', 'SUCCESS')->count(),
+            'transaction_in_cart' => Transaction::where('transaction_status', 'IN_CART')->count(),
+            'transaction_cancel' => Transaction::where('transaction_status', 'CANCEL')->count(),
+            'transaction_failed' => Transaction::where('transaction_status', 'FAILED')->count(),
+
+        ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -20,7 +20,7 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                     Paket Travel</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">7</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $travel_package }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-hotel fa-2x text-gray-300"></i>
@@ -38,7 +38,7 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                     Transaksi</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">14</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -58,7 +58,9 @@
                                 </div>
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">8</div>
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                            {{ $transaction_pending }}
+                                        </div>
                                     </div>
 
                                 </div>
@@ -79,7 +81,61 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                     Sukses</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_success }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-check fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                    In Cart</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_in_cart }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-check fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-muted shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-muted text-uppercase mb-1">
+                                    Cancel</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_cancel }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-check fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    Failed</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_failed }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-check fa-2x text-gray-300"></i>
@@ -94,5 +150,4 @@
 
     </div>
     <!-- /.container-fluid -->
-
 @endsection
